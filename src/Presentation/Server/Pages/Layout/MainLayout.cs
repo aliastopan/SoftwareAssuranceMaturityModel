d1dac16bc919b033b5c8253cca4df0b4c164c3cc
Taufan Augusta <taufan.augusta@protonmail.com>
2022-02-08 21:37:35 +0700

    add Role at AuthorizedView sidebar

---
+++ src/Presentation/Server/Pages/Layout/MainLayout.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoftwareAssuranceMaturityModel.Presentation.Server.Pages.Layout
+{
+    public partial class MainLayout
+    {
+
+    }
+}
