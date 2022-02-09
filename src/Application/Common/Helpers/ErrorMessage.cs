21b012566f019bfd056ad4763e821b770093416c
Taufan Augusta <taufan.augusta@protonmail.com>
2022-02-09 18:39:08 +0700

    add ErrorMessage

--- src/Application/Common/Helpers/ErrorMessage.cs
+++ src/Application/Common/Helpers/ErrorMessage.cs
@@ -4,5 +4,7 @@ namespace SoftwareAssuranceMaturityModel.Application.Common.Helpers
     {
         public const string USER_NOT_FOUND = "USER NOT FOUND";
         public const string INVALID_PASSWORD = "INVALID PASSWORD";
+        public const string NO_SESSION_FOUND = "NO_SESSION_FOUND";
+
     }
 }
