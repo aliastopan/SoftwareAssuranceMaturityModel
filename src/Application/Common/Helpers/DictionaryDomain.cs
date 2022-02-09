5b1824b87d8e86afeacb38712ce8bd9744f0968e
Taufan Augusta <taufan.augusta@protonmail.com>
2022-02-09 20:41:50 +0700

    add DictionaryEnum

---
+++ src/Application/Common/Helpers/DictionaryDomain.cs
@@ -0,0 +1,15 @@
+namespace SoftwareAssuranceMaturityModel.Application.Common.Helpers
+{
+    public static class DictionaryDomain
+    {
+        public static Dictionary<string, int> Domains = new()
+        {
+            {"D1", 1},
+            {"D2", 2},
+            {"D3", 3},
+            {"D4", 4},
+            {"D5", 5},
+            {"D6", 6}
+        };
+    }
+}
