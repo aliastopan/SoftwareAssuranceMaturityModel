516b5c725faecc2f5867f28f2b441dd9d01991b5
Taufan Augusta <taufan.augusta@protonmail.com>
2022-02-09 20:47:53 +0700

    add Respond Domain prop

--- src/Domain/Entities/Respond.cs
+++ src/Domain/Entities/Respond.cs
@@ -4,6 +4,7 @@ namespace SoftwareAssuranceMaturityModel.Domain.Entities
     public class Respond
     {
         public int Id { get; set; }
+        public int Domain { get; set; } = 0;
         public int Value { get; set; } = 0;
     }
 }
