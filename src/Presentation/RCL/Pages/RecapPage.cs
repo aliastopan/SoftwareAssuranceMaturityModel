ee4153f54b5acb403f07d52487466b80d151d2c8
Taufan Augusta <taufan.augusta@protonmail.com>
2022-02-10 15:10:23 +0700

    ready to graph

--- src/Presentation/RCL/Pages/RecapPage.cs
+++ src/Presentation/RCL/Pages/RecapPage.cs
@@ -1,3 +1,4 @@
+using System.Security.Authentication.ExtendedProtection;
 using Microsoft.AspNetCore.Components;
 using SoftwareAssuranceMaturityModel.Application.Common.Managers;
 
@@ -8,38 +9,53 @@ namespace SoftwareAssuranceMaturityModel.Presentation.RCL.Pages
         [Inject] private RecapManager _recapManager { get; set; } = default!;
 
         int _maxQ => _recapManager.MaxQ;
+        List<List<float>> _recapableResults = new();
+
+        protected void RecapCheck()
+        {
+            for (int i = 0; i < _recapableResults.Count; i++)
+            {
+                System.Console.WriteLine($"Domain #{i+1}");
+                for (int j = 0; j < _recapableResults[i].Count - 1; j++)
+                {
+                    System.Console.WriteLine($"Avg #{j+1}: {_recapableResults[i][j]} ");
+                }
+                int domainAvg = _recapableResults[i].Count - 1;
+                System.Console.WriteLine($"DOMAIN AVG {_recapableResults[i][domainAvg]}\n");
+
+            }
+        }
+
         protected override void OnAfterRender(bool firstRender)
         {
             if(firstRender)
             {
                 System.Console.WriteLine($"Total Q: {_maxQ}");
 
-                int q1 = _recapManager.QPerDomainCount(1);
-                int q2 = _recapManager.QPerDomainCount(2);
-                int q3 = _recapManager.QPerDomainCount(3);
-                int q4 = _recapManager.QPerDomainCount(4);
-                int q5 = _recapManager.QPerDomainCount(5);
-                int q6 = _recapManager.QPerDomainCount(6);
-
-                System.Console.WriteLine($"Q Count: {q1}");
-                System.Console.WriteLine($"Q Count: {q2}");
-                System.Console.WriteLine($"Q Count: {q3}");
-                System.Console.WriteLine($"Q Count: {q4}");
-                System.Console.WriteLine($"Q Count: {q5}");
-                System.Console.WriteLine($"Q Count: {q6}");
+                var recapable = _recapManager.GetAveragesPerDomain();
+                var _q1 = recapable[0];
 
 
-                // System.Console.WriteLine($"Q #{1} Avg: {_recapManager.Av}");
+                for (int i = 0; i < recapable.Count; i++)
+                {
+                    List<float> results = new();
+                    var respondsAvg = recapable[i];
+                    // System.Console.WriteLine($"Domain #{i+1}");
+                    for (int j = 0; j < respondsAvg.Count; j++)
+                    {
+                        float avg = respondsAvg[j].Value;
+                        results.Add(avg);
+                        // System.Console.WriteLine($"Avg #{j+1}: {avg} ");
+                    }
+                    var qAvg = _recapManager.AvgPerDomain(i+1);
+                    results.Add(qAvg);
+                    // System.Console.WriteLine($"DOMAIN AVG {qAvg}\n");
 
-        
-                // _recapManager.TotalRecap();
-                // var x = _recapManager.PerDomainAvg(1);
-                // System.Console.WriteLine($"Avg Per Domain: {x}");
-
-                // System.Console.WriteLine($"Recap QDomain: {_recapManager.AvgPerQ[2].QDomain}");
-                // System.Console.WriteLine($"Recap Value: {_recapManager.AvgPerQ[2].Value}");
-
+                    _recapableResults.Add(results);
+                }
             }
+        
+            RecapCheck();
         }
     }
 }
