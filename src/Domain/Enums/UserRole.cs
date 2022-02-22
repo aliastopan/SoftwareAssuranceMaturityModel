57ed58d8bc4b98aa84b7fd4d73864d9b9ef6107b
Taufan Augusta <taufan.augusta@protonmail.com>
2022-02-22 11:17:49 +0700

    fix: typo

--- src/Domain/Enums/UserRole.cs
+++ src/Domain/Enums/UserRole.cs
@@ -3,7 +3,7 @@ namespace SoftwareAssuranceMaturityModel.Domain.Enums
     public enum UserRole : byte
     {
         None = 0,
-        Sureyor = 1,
+        Surveyor = 1,
         Administrator = 2,
         Developer = 99
     }
