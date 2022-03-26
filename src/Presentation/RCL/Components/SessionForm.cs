3f797781bc81944370a123c13e101c0ccbfe9da1
Taufan Augusta <taufan.augusta@protonmail.com>
2022-03-26 18:48:20 +0700

    add force reload

--- src/Presentation/RCL/Components/SessionForm.cs
+++ src/Presentation/RCL/Components/SessionForm.cs
@@ -7,11 +7,13 @@ namespace SoftwareAssuranceMaturityModel.Presentation.RCL.Components
     public partial class SessionForm
     {
         [Inject] private CreateSession _createSession { get; init; } = default!;
+        [Inject] private NavigationManager _navManager { get; init; } = default!;
         private NewSessionDto _newSession { get; set; } = new();
 
         private async Task SubmitAsync()
         {
             await _createSession.Add(_newSession);
+            _navManager.NavigateTo("/administrator", forceLoad: true);
         }
 
     }
